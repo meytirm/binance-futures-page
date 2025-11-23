5c5f7c78648e1961409717c2e83f86900f88051f
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-23 14:56:26 +0330

    feat(utils): add icon color customization utility

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -1,7 +1,16 @@
-import type { ClassValue } from "clsx"
-import { clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import type { ClassValue } from 'clsx'
+import { clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const iconColorCustomize = (color: string) => {
+  const customize = (content: string) => {
+    return content
+      .replace(/stroke="[^"]*"/g, `stroke="${color}"`) // Change stroke color to red
+      .replace(/fill="[^"]*"/g, `fill="${color}"`) // Change fill color to red
+  }
+  return customize
 }

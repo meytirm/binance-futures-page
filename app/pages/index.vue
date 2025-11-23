e8badc3f8c1077e5541712e608a580aeba10a9d9
Mehdi Rahmani <mehdirahmani314@gmail.com>
2025-11-23 14:56:34 +0330

    feat(index): add customizable icon to index page

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     index
+    <Icon
+      name="b-arrow-left"
+      :customize="iconColorCustomize('red')"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { iconColorCustomize } from '~/lib/utils'
 </script>
 
 <style scoped>
